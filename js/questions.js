a5369ab8e815e67eed814092dcdd2b91b272208e
$yulo <101001858+yulocode@users.noreply.github.com>
2024-08-08 15:14:49 -0300

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "O material genético das células é :",
+    answer: "O ácido desoxirribonucleico.",
+    options: [
+      "Uma proteína.",
+      "A glicose",
+      "O ácido desoxirribonucleico.",
+      "O RNA"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Na célula, a função da respiração celular está reservada para:",
+    answer: "As mitocôndrias.",
+    options: [
+      "O aparelho de Golgi.",
+      "As mitocôndrias.",
+      "Os lisossomos.",
+      "O retículo endoplasmático."
+    ]
+  },
+    {
+    numb: 3,
+    question: "Os quatro grupos de moléculas orgânicas, também chamadas de biomoléculas são:",
+    answer: "Proteínas, lípides , carboidratos e ácido nucleicos.",
+    options: [
+      "Proteínas, lípides , carboidratos e ácido nucleicos.",
+      "Proteínas, aminoácidos, carboidratos e lisossomos.",
+      "Proteínas, glicose, carboidratos e lisossomos.",
+      "Proteínas, lípides, carboidratos e glicose."
+    ]
+  },
+    {
+    numb: 4,
+    question: "Uma célula bacteriana é desprovida de:",
+    answer: "Núcleo.",
+    options: [
+      "Ribossomos.",
+      "Parede celular.",
+      "DNA.",
+      "Núcleo."
+    ]
+  },
+    {
+    numb: 5,
+    question: "Os centríolos exercem importantes funções no processo de:",
+    answer: "Divisão celular",
+    options: [
+      "Secreção de substâncias",
+      "Transporte intracelular",
+      "Divisão celular",
+      "Respiração celular"
+    ]
+  },
+];
