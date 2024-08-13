8e6085060b199c440dc922b588d94d7da0f7070e
Silas Andrade <silasandrade.code@gmail.com>
2024-08-13 11:03:51 +0000

    main

--- js/script.js
+++ js/script.js
@@ -153,16 +153,16 @@ function showResult(){
     const scoreText = result_box.querySelector(".score_text");
     if (userScore > 8){ // if user scored more than 3
         //creating a new span tag and passing the user score number and total question number
-        let scoreTag = '<span>GÊNIO! 🤩, você acertou<p>' + userScore + '<p> de </p>' + questions.length + '<br> Jogo Criado por Yulo e Silas </p></span>' 
+        let scoreTag = '<span>GÊNIO! 🤩, você acertou<p>' + userScore + '<p> de </p>' + questions.length + '</p></span>' 
         scoreText.innerHTML = scoreTag
     }
     else if(userScore >= 6){ // if user scored more than 1
-        let scoreTag = '<span>boa 😎, você acertou <p>'+ userScore +'</p> de <p>'+ questions.length +'<br> Jogo Criado por Yulo e Silas</p></span>';
+        let scoreTag = '<span>boa 😎, você acertou <p>'+ userScore +'</p> de <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
     }
   
     else if(userScore <= 5){ // if user scored less than 1
-        let scoreTag = '<span>MUITOOO RUIM!! 😂😂<p>'+ userScore +'</p> de <p>'+ questions.length +'<br> Jogo Criado por Yulo e Silas </p></span>';
+        let scoreTag = '<span>MUITOOO RUIM!! 😂😂<p>'+ userScore +'</p> de <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
     }
     scoreText.innerHTML = scoreTag
