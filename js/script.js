01f2b3a129f7101f965e2a7705ba0b365ce93198
Silas Andrade <silasandrade.code@gmail.com>
2024-08-13 11:13:55 +0000

    main

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ const quit_quiz = result_box.querySelector(".buttons .quit");
 restart_quiz.onclick = ()=>{
     quiz_box.classList.add("activeQuiz"); //show quiz box
     result_box.classList.remove("activeResult"); //hide result box
-    timeValue = 15;
+    timeValue = 30;
     que_count = 0;
     que_numb = 1;
     userScore = 0;
